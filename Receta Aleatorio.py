3d26d47729fac942a65598462882ce12900b7d46
jmverdefray <32619831+jmverdefray@users.noreply.github.com>
2017-12-20 09:06:52 +0100

    Pequeña receta

---
+++ Receta Aleatorio.py
@@ -0,0 +1,11 @@
+# esto se añade al principio del archivo
+
+import random2 # learn more: https://python.org/pypi/random2
+
+
+
+## Asi se saca un numero aleatorio entero, entre los dos límites dados 
+
+x=random2.randint(0,4)
+
+print(x)
